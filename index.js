89f3ecfe8db677b3fd4b1b6b2e471b31294ec0e9
Drake Hopkins <drakehopkins@proton.me>
2023-02-28 07:27:08 -0600

    updates

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ window.addEventListener('DOMContentLoaded', () => {
       backgroundColor: 0x121212,
       points: 20.00
       })
+      setTimeout(() => {
+        const main = document.querySelector("main");
+        main.style.opacity = 1;
+        main.style.filter = "blur(0px)";
+      }, 1000);
   })
