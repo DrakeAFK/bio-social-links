21a11795354be9ca83132b91fd4ade2684e1bf6e
Drake Hopkins <drakehopkins@proton.me>
2023-02-28 03:28:17 -0600

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.NET({
+      el: "#vanta",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xff6e,
+      backgroundColor: 0x121212,
+      points: 20.00
+      })
+  })
